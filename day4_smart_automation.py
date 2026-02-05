9093782cb6548232fc547ee085401be779203230
newtech290729-oss <newtech290729@gmail.com>
2026-02-04 19:43:23 -0800

    Add files via upload
    “Day 4 – Input validation and smart decision automation”

---
+++ day4_smart_automation.py
@@ -0,0 +1,40 @@
+days_input = input("Enter number of days you worked this week: ")
+
+if not days_input.isdigit():
+    print("Error: Days must be a whole number (no decimals, no text).")
+    exit()
+
+days = int(days_input)
+
+if days == 0:
+    print("Error: Days worked cnanot be zero.")
+    exit()
+
+print("Days", days)
+
+total_earnings = 0.0
+
+for day in range(1, days +1):
+    earning_input = input(f"Enter earnings for day {day}: ")
+
+    try:
+        earning = float(earning_input)
+    except ValueError:
+        print("ERROR: Earnings must be a number:")
+        exit()
+
+    total_earnings += earning
+    print("Total", total_earnings)
+
+    average = total_earnings / days
+
+print("\n==== WEEKLY SUMMARY====")
+print("Total Earnings: ", round(total_earnings, 2))
+print("Average per day: ", round(average, 2))
+
+if average < 150:
+    print("⚠️Alert: Earnings are below expected level.")
+elif average > 300:
+    print("🔥Great job! High earning week.")
+else:
+    print("👍Earnings are in normal range.")
